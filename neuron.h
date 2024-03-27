6e5f351685959776b61dad8e88ada53f63673c3f
malek nasraoui <148629570+maleknas@users.noreply.github.com>
2024-03-27 13:16:24 +0100

    Update neuron.h

--- neuron.h
+++ neuron.h
@@ -1,21 +1,37 @@
 #ifndef NEURAL_NETWORK_H_INCLUDED
 #define NEURAL_NETWORK_H_INCLUDED
-#include<stdio.h>
-#include<math.h>
-struct neurone {          //neurone
-float b;                    //bias
-float a;                    //activation
-float w;                    //weight
-}; typedef struct neurone neur ;
+
+#include <stdio.h>
+#include <math.h>
+
+struct neuron {
+    double b;  // Bias
+    double a;  // Activation
+    double w;  // Weight
+};
+typedef struct neuron neuron;
+
 struct layer {
-float n;                    //nombre de neutrone
-neur *t;                  //tableau de neuronne de taille n
-}; typedef struct layer l;
-layer *network;
-//segmoid function
-int segm(int z){
-int res;
-res=1/(1+exp(-z));
-return res;
+    int n;    // Number of neurons
+    neuron *t;  // Array of neurons of size n
+};
+typedef struct layer layer;
+
+// Sigmoid function
+double sigmoid(double z) {
+    return 1 / (1 + exp(-z));
 }
+
+// Loss function
+// - outl : output layer
+// - y    : wanted output
+// - m    : number of samples
+double loss(neuron *outl, int *y, int m) {
+    double l = 0;
+    for (int i = 0; i =< m; i++) {
+        l -= y[i] * log(outl[i].a) + (1 - y[i]) * log(1 - outl[i].a);
+    }
+    return l / m; // Return the average loss
+}
+
 #endif // NEURAL_NETWORK_H_INCLUDED
