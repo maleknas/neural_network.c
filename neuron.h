5714d60aec2e492d4a24d733b8641ef74181f5cb
malek nasraoui <148629570+maleknas@users.noreply.github.com>
2024-04-02 14:23:08 +0100

    Update neuron.h

--- neuron.h
+++ neuron.h
@@ -35,29 +35,3 @@ double loss(neuron *outl, int *y, int m) {
 }
 
 #endif // NEURAL_NETWORK_H_INCLUDED
-typedef struct neuron neuron;
-
-struct layer {
-    int n;    // Number of neurons
-    neuron *t;  // Array of neurons of size n
-};
-typedef struct layer layer;
-
-// Sigmoid function
-double sigmoid(double z) {
-    return 1 / (1 + exp(-z));
-}
-
-// Loss function
-// - outl : output layer
-// - y    : wanted output
-// - m    : number of samples
-double loss(neuron *outl, int *y, int m) {
-    double l = 0;
-    for (int i = 0; i =< m; i++) {
-        l -= y[i] * log(outl[i].a) + (1 - y[i]) * log(1 - outl[i].a);
-    }
-    return l / m; // Return the average loss
-}
-
-#endif // NEURAL_NETWORK_H_INCLUDED
