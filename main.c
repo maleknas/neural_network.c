d57358d71af4261c489341e4edf743945ec4505f
malek nasraoui <148629570+maleknas@users.noreply.github.com>
2024-04-02 14:24:36 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "neural_network.h"
+#include "initialisation_network.h"
+#include "test_network.h"
+
+int main() {
+    int numlayer, *numneur;
+    // Input the number of layers
+    printf("Enter the number of layers: ");
+    scanf("%d", &numlayer);
+
+    // Input the number of neurons for each layer
+    printf("Enter the number of neurons for each layer:\n");
+    numneur = (int *)malloc(numlayer * sizeof(int));
+    for (int i = 0; i < numlayer; i++) {
+        printf("Layer %d: ", i + 1);
+        scanf("%d", &numneur[i]);
+    }
+
+    // Initialize the network
+    layer *net = (layer *)malloc(numlayer * sizeof(layer));
+    init_network(net, numlayer, numneur);
+
+    // Free dynamically allocated memory
+    free(numneur);
+    free(net);
+
+    // Print the details of neurons in each layer
+    for(int i = 0; i <2; i++) {
+        printf("Details of neurons in Layer %d:\n", i + 1);
+        print_layer(net[3]);
+    }
+
+    return 0;
+}
