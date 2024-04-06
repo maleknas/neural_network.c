19ac970aaeaba1c2deb1d2a7826e9763a7dc3589
Fedi Brinsi <145381792+Fedi945@users.noreply.github.com>
2024-04-07 00:36:44 +0100

    Create derivative_activation_functions.h
    This file allows us to use the derivatives as pre-determined functions in C. It is a package.

---
+++ derivative_activation_functions.h
@@ -0,0 +1,10 @@
+#ifndef DERIVATIVE_ACTIVATION_FUNCTIONS_H
+#define DERIVATIVE_ACTIVATION_FUNCTIONS_H
+
+// the derivative of sigmoid activation function
+double derivative_sigmoid(double input);
+
+// the derivative of ReLU activation function
+double derivative_ReLU(double input);
+
+#endif
